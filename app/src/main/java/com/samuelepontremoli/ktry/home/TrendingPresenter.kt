5effdf3d84c0774bbe910d0b6e7f84b0f3d26800
samuele <samuele.pontremoli@gmail.com>
2017-07-08 17:55:57 +0200

    Added MVP architecture for trending fragment

---
+++ app/src/main/java/com/samuelepontremoli/ktry/home/TrendingPresenter.kt
@@ -0,0 +1,37 @@
+package com.samuelepontremoli.ktry.home
+
+import com.samuelepontremoli.ktry.network.GiphyRepositoryProvider
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by samuele on 08/07/17.
+ * Trending gifs presenter
+ */
+class TrendingPresenter(val view: ITrendingContract.ITrendingView) : ITrendingContract.ITrendingPresenter {
+
+    private var subscriptions = CompositeDisposable()
+
+    override fun loadTrending() {
+        //Set api repository
+        val giphyRepository = GiphyRepositoryProvider.provideGiphyRepository()
+
+        //Subscribe to repository call
+        val trendingFlow = giphyRepository.getTrending()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    (data) ->
+                    view.onTrendingLoadedSuccess(data)
+                }, {
+                    error ->
+                    view.onTrendingLoadedFailure(error)
+                }, {
+                    view.refreshTrending()
+                })
+
+        subscriptions.add(trendingFlow)
+    }
+
+}
